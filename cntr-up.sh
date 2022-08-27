56782de7810f364b594413ac86b9b4d542a86e40
Ashain <ashainrox@gmail.com>
2022-08-27 01:11:01 +0000

    scr

--- cntr-up.sh
+++ cntr-up.sh
@@ -2,4 +2,4 @@ theDomain="staging.hugsintl.com"
 
 
 # Build
-docker build --no-cache -t $theDomain . && docker run -p 6060:80 -d --name $theDomain $theDomain:latest
+docker build --no-cache -t $theDomain . && docker run -p 80:80 -d --name $theDomain $theDomain:latest
---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<center><a href='#' class='underline text-gray-900 dark:text-white'>{{request()->secure()?'https':'http'}} {{config('app.name')}}</a></center>
